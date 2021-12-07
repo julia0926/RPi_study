1f8e9d7e204aee126c04593cb15bbbfac54619c3
kimjiin <kj980926@naver.com>
2021-12-07 16:17:28 +0900

    [Py] 초음파 센서 제어 (주석추가)

--- Ultrasonic_waves/hc_sr04.py
+++ Ultrasonic_waves/hc_sr04.py
@@ -4,8 +4,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO_TRIGGER = 17
 GPIO_ECHO = 27
 print("Ultrasonic Distance Measurement")
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)
-GPIO.setup(GPIO_ECHO,GPIO.IN)
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT) #초음파로 내보낼 트리거 핀 = 출력모드 
+GPIO.setup(GPIO_ECHO,GPIO.IN) #반사판을 수신할 에코핀 = 입력모드 
 
 try:
 	while True:
@@ -16,15 +16,16 @@ try:
 		GPIO.output(GPIO_TRIGGER,True)
 		time.sleep(0.00001)
 		GPIO.output(GPIO_TRIGGER,False)
-		
+		#에코핀이 on일 시점을 시작으로 
 		while GPIO.input(GPIO_ECHO)==0:
 			start=time.time()
-		
+		#에코핀이 다시 off되는 시점을 반사파 수신시간
 		while GPIO.input(GPIO_ECHO)==1:
-			start=time.time()
-			
+			stop=time.time()
+		#계산
 		elapsed=stop-start
-		
+
+		#초음파는 반사파이기 때문에 실제 이동거리는 2배이므로 2로 다시 나눔
 		if(stop and start):
 			distance = (elapsed*34000.0)/2
 			print("Distance :%.1f cm" % distance)
